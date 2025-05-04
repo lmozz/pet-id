cf8b67f75aead3b950cbd1a9ca8754e9e8610f74
moz <lhgonzalezmoz@hotmail.com>
2025-05-04 00:09:21 -0600

    agregando pagina de ganesha

---
+++ static/js/petid.js
@@ -0,0 +1,66 @@
+(() => {
+    setTimeout(() => {
+        document.getElementsByTagName('body')[0].removeAttribute('style');
+    }, 1000);
+    new Swiper('.slider', {
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+        effect: 'cube',
+        grabCursor: true,
+        cubeEffect: {
+            shadow: true,
+            slideShadows: true,
+            shadowOffset: 20,
+            shadowScale: 0.94,
+        },
+    });
+    new Swiper(".target", {
+        effect: "cards",
+        grabCursor: true,
+    });
+    new Swiper(".parallax", {
+        speed: 600,
+        parallax: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+    ScrollReveal({ reset: true });
+    ScrollReveal().reveal('.scroll-animation', {
+        delay: 0,
+        distance: '200px',
+        duration: 2000,
+        origin: 'bottom',
+        rotate: {
+            x: 0,
+            y: 60,
+            z: 20,
+        },
+    });
+    document.addEventListener('contextmenu', function (e) {
+        e.preventDefault();
+    });
+    document.addEventListener('keydown', function (e) {
+        if ((e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J')) ||
+            (e.key === 'F12') ||
+            (e.ctrlKey && e.key === 'U')) {
+            e.preventDefault();
+            return false;
+        }
+    });
+})();
