24bedba37d32e966e958aedb5f5985bdff3c35b4
moz <lhgonzalezmoz@hotmail.com>
2025-05-04 00:19:44 -0600

    pagina index terminada

---
+++ static/js/index.js
@@ -0,0 +1,16 @@
+(() => {
+    document.addEventListener('contextmenu', e => {
+        e.preventDefault();
+    });
+    document.addEventListener('keydown', e => {
+        if ((e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J')) ||
+            (e.key === 'F12') ||
+            (e.ctrlKey && e.key === 'U')) {
+            e.preventDefault();
+            return false;
+        }
+    });
+    document.addEventListener('dragstart', e => {
+        e.preventDefault();
+    });
+})();
--- static/js/petid.js
+++ static/js/petid.js
@@ -52,15 +52,18 @@
             z: 20,
         },
     });
-    document.addEventListener('contextmenu', function (e) {
+    document.addEventListener('contextmenu', e => {
         e.preventDefault();
     });
-    document.addEventListener('keydown', function (e) {
+    document.addEventListener('keydown', e => {
         if ((e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J')) ||
             (e.key === 'F12') ||
             (e.ctrlKey && e.key === 'U')) {
             e.preventDefault();
             return false;
         }
+    });
+    document.addEventListener('dragstart', e => {
+        e.preventDefault();
     });
 })();
